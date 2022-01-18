d91070b6378222f3aea8ef8cb30eb8a6cd4b0259
dudqo225 <dudqo225@gmail.com>
2022-01-18 14:49:14 +0900

    Feat : User Entity 생성

---
+++ src/main/java/com/ssafy/ssafymate/entity/User.java
@@ -0,0 +1,61 @@
+package com.ssafy.ssafymate.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String campus;
+
+    @NotNull
+    private String ssafyTrack;
+
+    @NotNull
+    private String studentNumber;
+
+    @NotNull
+    private String studentName;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    private String profileImg;
+
+    @NotNull
+    private String selfIntroduction;
+
+    @NotNull
+    private String job1;
+
+    private String job2;
+
+    @NotNull
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    List<TechStack> techStacks = new ArrayList<>();
+
+    private String githubUrl;
+
+    private String etcUrl;
+
+    @NotNull
+    private Boolean agreement;
+
+}

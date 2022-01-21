b2681513842761ad068b47a4d34492e2f4e81f8f
dudqo225 <dudqo225@gmail.com>
2022-01-21 14:49:24 +0900

    Feat : 비밀번호 찾기 기능 구현

--- src/main/java/com/ssafy/ssafymate/service/EmailService.java
+++ src/main/java/com/ssafy/ssafymate/service/EmailService.java
@@ -2,5 +2,6 @@ package com.ssafy.ssafymate.service;
 
 public interface EmailService {
     String sendSimpleMessage(String to) throws Exception;
+    String sendPwSimpleMessage(String to) throws Exception;
     Long getUserIdByCode(String email, String ePw);
 }
--- src/main/java/com/ssafy/ssafymate/service/UserService.java
+++ src/main/java/com/ssafy/ssafymate/service/UserService.java
@@ -1,5 +1,6 @@
 package com.ssafy.ssafymate.service;
 
+import com.ssafy.ssafymate.dto.request.PwModifyRequestDto;
 import com.ssafy.ssafymate.dto.request.UserRequestDto;
 import com.ssafy.ssafymate.entity.User;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,7 +8,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 public interface UserService {
+    User getUserById(Long id);
     User getUserByEmail(String email);
     User userSave(UserRequestDto userRequestDto, MultipartFile multipartFile) throws IOException;
     String getEmailByStudentNumberAndStudentName(String studentNumber, String studentName);
+    User passwordModify(PwModifyRequestDto pwModifyRequestDto, User user);
 }

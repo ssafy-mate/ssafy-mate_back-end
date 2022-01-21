14e28e3be86c493609af63a92698eb1083d8ff6b
dudqo225 <dudqo225@gmail.com>
2022-01-21 14:49:24 +0900

    Feat : 비밀번호 찾기 기능 구현

--- src/main/java/com/ssafy/ssafymate/repository/UserRepository.java
+++ src/main/java/com/ssafy/ssafymate/repository/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
     Optional<User> findByEmail(String email);
     Optional<User> findEmailByStudentNumberAndStudentName(String studentNumber, String studentName);
 }

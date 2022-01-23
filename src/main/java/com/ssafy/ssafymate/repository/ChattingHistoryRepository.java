aa9d45955c50b3c6b1e5b0cee8d7311b0b90ac02
Jo-wonbin <78211128+Jo-wonbin@users.noreply.github.com>
2022-01-24 04:42:09 +0900

    Feat : 채팅 연결 API

--- src/main/java/com/ssafy/ssafymate/repository/ChattingHistoryRepository.java
+++ src/main/java/com/ssafy/ssafymate/repository/ChattingHistoryRepository.java
@@ -4,9 +4,12 @@ import com.ssafy.ssafymate.dto.ChatDto.ContentList;
 import com.ssafy.ssafymate.entity.ChattingHistory;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,4 +22,9 @@ public interface ChattingHistoryRepository extends JpaRepository<ChattingHistory
             "on U.id = CH.sender_id\n" +
             "WHERE CH.chatting_room_id = :roomId", nativeQuery = true)
     Optional<List<ContentList>> getHistoryList(@Param("roomId") String roomId);
+
+    @Modifying
+    @Query(value = "insert into chatting_history(room_id, sender_id, sent_time, content) values (:roomId, :senderId, :sentTime, :content)", nativeQuery = true)
+    @Transactional
+    int saveHistory(@Param("roomId") String roomId, @Param("senderId") Long senderId, @Param("sentTime") LocalDateTime sentTime, @Param("content") String content);
 }

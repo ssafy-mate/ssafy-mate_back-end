c449c30347677ffa36551816ebc7d5833cd6bab0
dudqo225 <dudqo225@gmail.com>
2022-01-24 10:32:52 +0900

    Modify : 배포를 위한 코드 수정

--- src/test/java/com/ssafy/ssafymate/SsafyMateBackEndApplicationTests.java
+++ src/test/java/com/ssafy/ssafymate/SsafyMateBackEndApplicationTests.java
@@ -1,9 +1,8 @@
 package com.ssafy.ssafymate;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SsafyMateBackEndApplicationTests {
 
 	@Test

acda46ae02349f3af7ec379f98b6306d98d6240c
dudqo225 <dudqo225@gmail.com>
2022-02-05 14:38:04 +0900

    Modify : 유저 관련 코드 수정 null point exception 캐치

--- src/main/java/com/ssafy/ssafymate/JWT/CustomUserDetailsService.java
+++ src/main/java/com/ssafy/ssafymate/JWT/CustomUserDetailsService.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
-@Service
+@Service("customUserDetailService")
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired

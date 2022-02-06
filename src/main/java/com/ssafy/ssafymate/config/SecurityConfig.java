39ce816de314e9888237e1fbb3948c988632fbd7
dudqo225 <dudqo225@gmail.com>
2022-02-06 17:00:09 +0900

    Modify : Cors 설정:

--- src/main/java/com/ssafy/ssafymate/config/SecurityConfig.java
+++ src/main/java/com/ssafy/ssafymate/config/SecurityConfig.java
@@ -76,9 +76,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();

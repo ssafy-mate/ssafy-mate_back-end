f9c3199de96bc51b339c39f05ed87b6caae3215b
dudqo225 <dudqo225@gmail.com>
2022-02-06 15:21:04 +0900

    Modify : https 관련 코드 추가

--- src/main/java/com/ssafy/ssafymate/SsafyMateBackEndApplication.java
+++ src/main/java/com/ssafy/ssafymate/SsafyMateBackEndApplication.java
@@ -1,9 +1,13 @@
 package com.ssafy.ssafymate;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SsafyMateBackEndApplication extends SpringBootServletInitializer {
@@ -17,4 +21,16 @@ public class SsafyMateBackEndApplication extends SpringBootServletInitializer {
 		SpringApplication.run(SsafyMateBackEndApplication.class, args);
 	}
 
+	@Bean
+	public ServletWebServerFactory servletContainer(){
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector(){
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8081);
+		return connector;
+	}
 }

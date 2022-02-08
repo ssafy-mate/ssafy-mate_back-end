34e702c5a720fe791a151c7f80ae51e8aaf54ab8
dudqo225 <dudqo225@gmail.com>
2022-02-08 17:17:52 +0900

    Refactor : Jwt 토큰 코드 리팩토링

--- src/main/java/com/ssafy/ssafymate/JWT/TokenProvider.java
+++ src/main/java/com/ssafy/ssafymate/JWT/TokenProvider.java
@@ -74,7 +74,8 @@ public class TokenProvider {
     // 토큰의 유효성 + 만료일자 확인
     public boolean validateToken(String jwtToken) {
         try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            String subToken = jwtToken.substring(7, jwtToken.length());
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(subToken);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (Exception e) {
             return false;

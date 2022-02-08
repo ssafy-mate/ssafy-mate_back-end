8bb8b4bda78880256fd2262b345da93518043714
dudqo225 <dudqo225@gmail.com>
2022-02-08 17:17:52 +0900

    Refactor : Jwt 토큰 코드 리팩토링

--- src/main/java/com/ssafy/ssafymate/JWT/JwtFilter.java
+++ src/main/java/com/ssafy/ssafymate/JWT/JwtFilter.java
@@ -23,10 +23,8 @@ public class JwtFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 헤더에서 JWT 를 받아옵니다.
-        String bearerToken = tokenProvider.resolveToken((HttpServletRequest) request);
-        String[] tokenList = bearerToken.split(" ");
-        String token = tokenList[1];
+//        // 헤더에서 JWT 를 받아옵니다.
+        String token = tokenProvider.resolveToken((HttpServletRequest) request);
         // 유효한 토큰인지 확인합니다.
         if (token != null && tokenProvider.validateToken(token)) {
             // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
@@ -34,9 +32,7 @@ public class JwtFilter extends GenericFilterBean {
             // SecurityContext 에 Authentication 객체를 저장합니다.
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-//        else {
-//            throw new JwtException();
-//        }
+
         chain.doFilter(request, response);
     }
 }

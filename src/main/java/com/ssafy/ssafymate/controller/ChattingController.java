d3a2a6a6e3bb19ffa5e488ad156cf7a4973745a5
Jo-wonbin <jwb7214@gmail.com>
2022-02-08 16:54:08 +0900

    Modify : url 경로설정

--- src/main/java/com/ssafy/ssafymate/controller/ChattingController.java
+++ src/main/java/com/ssafy/ssafymate/controller/ChattingController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/api/chats")
 public class ChattingController {
 
     @Autowired

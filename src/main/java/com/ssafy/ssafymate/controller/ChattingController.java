d839685f53d46917660c92ba7dcd3d2f41922647
Jo-wonbin <jwb7214@gmail.com>
2022-03-16 20:48:57 +0900

    Modify : 채팅 API 예외처리문 추가

--- src/main/java/com/ssafy/ssafymate/controller/ChattingController.java
+++ src/main/java/com/ssafy/ssafymate/controller/ChattingController.java
@@ -8,11 +8,13 @@ import com.ssafy.ssafymate.dto.response.ChatRoomResponseDto;
 import com.ssafy.ssafymate.dto.response.ChatUserResponseDto;
 import com.ssafy.ssafymate.entity.User;
 import com.ssafy.ssafymate.service.ChattingService;
+import com.ssafy.ssafymate.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +25,9 @@ public class ChattingController {
 
     @Autowired
     private ChattingService chattingService;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/infos/{userId}")
     @ApiOperation(value = "상대방 이미지 불러오기", notes = "상대방의 이름과 사진을 불러온다.")
@@ -39,7 +44,7 @@ public class ChattingController {
 
             if (user == null) {
 
-                return ResponseEntity.status(400).body(ErrorResponseBody.of(400, false, "해당 사용자가 존재하지 않습니다."));
+                return ResponseEntity.status(400).body(ErrorResponseBody.of(400, false, "상대방 정보를 불러올 수 없습니다."));
 
             }
 
@@ -47,7 +52,7 @@ public class ChattingController {
 
         } catch (Exception e) {
 
-            return ResponseEntity.status(500).body(ErrorResponseBody.of(500, false, "서버 에러가 발생했습니다."));
+            return ResponseEntity.status(500).body(ErrorResponseBody.of(500, false, "Internal Server Error, 상대방 정보 불러오기 실패"));
 
         }
 
@@ -57,13 +62,21 @@ public class ChattingController {
     @ApiOperation(value = "채팅방 리스트 불러오기", notes = "사용자가 대화한 채팅방 리스트를 불러온다.")
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 400, message = "인증 실패"),
+            @ApiResponse(code = 400, message = "조회 실패"),
+            @ApiResponse(code = 401, message = "인증 실패"),
             @ApiResponse(code = 403, message = "토큰 인증 실패"),
             @ApiResponse(code = 500, message = "서버 오류")
     })
-    public ResponseEntity<?> getRoomList(@PathVariable("userId") Long userId) {
+    public ResponseEntity<?> getRoomList(@PathVariable("userId") Long userId, @AuthenticationPrincipal final String token) {
 
         try {
+
+            if (token == null) {
+                return ResponseEntity.status(401).body(ErrorResponseBody.of(401, false, "로그인 후 이용 가능합니다."));
+            }
+
+            User sender = userService.getUserByEmail(token);
+
             List<RoomList> roomList = chattingService.getRoomList(userId);
 
             if (roomList == null) {
@@ -74,7 +87,7 @@ public class ChattingController {
 
         } catch (Exception e) {
 
-            return ResponseEntity.status(500).body(ErrorResponseBody.of(500, false, "서버 에러가 발생했습니다."));
+            return ResponseEntity.status(500).body(ErrorResponseBody.of(500, false, "Internal Server Error, 대화 목록 불러오기 실패"));
 
         }
 
@@ -91,16 +104,24 @@ public class ChattingController {
     })
     public ResponseEntity<?> getHistoryList(
             @PathVariable("roomId") String roomId,
-            @RequestParam("nextCursor") Long messageId
+            @RequestParam("nextCursor") Long messageId,
+            @AuthenticationPrincipal final String token
     ) {
+
+        if (token == null) {
+            return ResponseEntity.status(401).body(ErrorResponseBody.of(401, false, "로그인 후 이용 가능합니다."));
+        }
 
         if(roomId.equals("null")){
 
-            return ResponseEntity.status(401).body(ErrorResponseBody.of(401, false, "선택된 대화 내용이 없습니다."));
+            return ResponseEntity.status(401).body(ErrorResponseBody.of(400, false, "선택된 대화 내용이 없습니다."));
 
         }
 
         try {
+
+
+            User sender = userService.getUserByEmail(token);
 
             String[] ids = roomId.split("-");
 
@@ -124,7 +145,7 @@ public class ChattingController {
 
         } catch (Exception e) {
 
-            return ResponseEntity.status(500).body(ErrorResponseBody.of(500, false, "서버 에러가 발생했습니다."));
+            return ResponseEntity.status(500).body(ErrorResponseBody.of(500, false, "Internal Server Error, 대화 내용 불러오기 실패"));
 
         }
 
